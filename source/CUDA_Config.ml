695a2e6c6b677d118e48e392f41fe3a32b9c5148
Stefan Muller <smuller2@iit.edu>
2022-11-18 13:24:57 -0600

    Use the CUDA abstraction domain and not just Presburger to get boundsThis should fix the issue of BlockDim not showing up in the bounds.

--- source/CUDA_Config.ml
+++ source/CUDA_Config.ml
@@ -8,6 +8,10 @@ let bidx_var = "__bidx"
 let bidy_var = "__bidy"
 let bidz_var = "__bidz"
 let bid_var = "__bid"
+let bdimx_var = "__bdimx"
+let bdimy_var = "__bdimy"
+let bdimz_var = "__bdimz"
+              
 let warpsize_var = "__warp"
 
 let div_cost = "__div_cost"
@@ -19,5 +23,8 @@ let cuda_vars = [tid_var;
                  bidx_var;
                  bidy_var;
                  bidz_var;
+                 bdimx_var;
+                 bdimy_var;
+                 bdimz_var;
                  bid_var;
                  warpsize_var]
