5cdeb89301d569c1d549c08628ae083654e806e8
Stefan Muller <smuller2@iit.edu>
2023-01-19 16:30:26 -0600

    Why doesn't it find bounds for single vars?

--- kernels/addSub1BoundsFailure3.cu
+++ kernels/addSub1BoundsFailure3.cu
@@ -3,8 +3,8 @@ __global__ void addSubArray1 (int *A, int *B, int w, int h) {
   //if (h > 4) {
     for (int i = 0; i < w; i++) {
       int j = blockIdx.x * blockDim.x + threadIdx.x;
-      B[2 * j * w + i] += A[i];
-      B[(2 * j + 1) * w + i] -= A[i];
+      B[2 * j * w + i] += A[i + 0];
+      B[(2 * j + 1) * w + i] -= A[i + 0];
     }
   //} else {
     for (int i = 0; i < 5; i++) {
@@ -12,8 +12,10 @@ __global__ void addSubArray1 (int *A, int *B, int w, int h) {
       //int j = 9 - 3 + k * 2;
       //A[i] = 1;
       //B[i] = 1;
-      B[i] += A[i];
-      B[i] -= A[i];
+      B[i + 0] += A[i + 0];
+      B[i + 0] -= A[i + 0];
+      //A[i] += A[i];
+      //B[i] += A[i];
     }
   //}
   //B[w] = 0;
